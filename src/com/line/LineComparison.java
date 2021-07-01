4c286d5aa2eea840de5acd09d7a621c822d19149
unknown <shaikh.ayan737@gmail.com>
2021-07-01 22:21:04 +0530

    variable declared

--- src/com/line/LineComparison.java
+++ src/com/line/LineComparison.java
@@ -4,6 +4,13 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparison computation program");
+		int x1,x2,y1,y2;
+		x1=5;
+		x2=10;
+		y1=8;
+		y2=10;
+		double len1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println(len1);
 
 	}
 
