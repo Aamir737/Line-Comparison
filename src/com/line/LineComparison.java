7028f25dd931883f0221f089cf0a3f82ff865b53
unknown <shaikh.ayan737@gmail.com>
2021-07-01 22:39:46 +0530

    second line added

--- src/com/line/LineComparison.java
+++ src/com/line/LineComparison.java
@@ -9,9 +9,17 @@ public class LineComparison {
 		x2=10;
 		y1=8;
 		y2=10;
+		
+		int a1,a2,b1,b2;
+		a1=5;
+		a2=10;
+		b1=8;
+		b2=10;
+		
 		double len1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		System.out.println(len1);
-
+		double len2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+		System.out.println(len2);
 	}
 
 }
