a81d85f966baa02a159d804cb0538207d7995565
unknown <shaikh.ayan737@gmail.com>
2021-07-01 22:48:24 +0530

    lines are equal or not

--- src/com/line/LineComparison.java
+++ src/com/line/LineComparison.java
@@ -14,12 +14,20 @@ public class LineComparison {
 		a1=5;
 		a2=10;
 		b1=8;
-		b2=10;
+		b2=20;
 		
 		double len1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		System.out.println(len1);
 		double len2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
 		System.out.println(len2);
+		
+		if (len1==len2) {
+			System.out.println("the length of line1 and line2 are equal:" );
+		}
+			else
+			
+			{
+				System.out.println("the length of line1 and line2 are not equal:" );
+			}
+		}
 	}
-
-}
